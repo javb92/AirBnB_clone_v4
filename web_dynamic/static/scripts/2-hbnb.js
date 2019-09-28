e77b52b6a3426a818602218bd1554c9ad180b98c
javb92 <mentor37_@hotmail.com>
2019-09-28 03:32:13 +0000

    [TEST]

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
   let AmenityChecked = [];
   $('input[type=checkbox]').change(function () {
-    let name = $(this).attr('data-name');
+    const name = $(this).attr('data-name');
     if ($(this).is(':checked')) {
       AmenityChecked.push(name);
-      console.log(AmenityChecked);
     } else {
       AmenityChecked = AmenityChecked.filter(Amenity => Amenity !== name);
-      console.log(AmenityChecked);
     }
     $('.amenities h4').text(AmenityChecked.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (status === 'success') {
+      $('DIV#api_status').toggleClass('available');
+    }
+    console.log(status);
   });
 });
