e2d243e11c8313ca62933ec8a24da8a0c7e95228
javb92 <mentor37_@hotmail.com>
2019-10-01 08:55:09 +0000

    [FIX] semistandard

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -32,19 +32,23 @@ $(document).ready(function () {
     $.each($("input[type='checkbox']:checked"), function () {
       checkons[($(this).attr('data-id'))] = $(this).attr('data-name');
     });
-      $('.places article').remove();
-      const amen_Param = { amenities: Object.keys(checkons) };
-      $.ajax({
-	  type: 'POST',
-	  url: 'http://localhost:5001/api/v1/places_search/',
-	  data: JSON.stringify(amen_Param),
-	  contentType: 'application/json',
-	  success: function (data) {
-              for (let i = 0; i < data.length; i++) {
-		  $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
-              }
-	  }
-      });
-      console.log(amen_Param);
+    $('.places article').remove();
+    const amenParam = { amenities: Object.keys(checkons) };
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search/',
+      data: JSON.stringify(amenParam),
+      contentType: 'application/json',
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night +
+'</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' +
+data[i].max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' +
+data[i].number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' +
+data[i].number_bathrooms + ' Bathroom</div></div><div class="description">' + data[i].description + '</div></article>');
+        }
+      }
+    });
+    console.log(amenParam);
   });
 });
